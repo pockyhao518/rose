64a0a00efd08760a10bbbd92a084de77f25f0d93
Hao <pockyhao518@gmail.com>
2021-07-15 02:18:29 -0400

    app front

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,8 +2,10 @@ import React from "react";
 import { Route } from 'react-router-dom';
 import SplashPage from "./splash_page";
 
-export default App = () => (
+const App = () => (
     <div>
         <Route exact path="/" component={SplashPage} />
     </div>
 );
+
+export default App;
