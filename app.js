2839702acf689e1c0d6b25f458b59f3f472a3921
Hao <pockyhao518@gmail.com>
2021-07-15 01:46:43 -0400

    remove hello

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ mongoose
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+// app.get("/", (req, res) => res.send("Hello World"));
 app.use("/api/datas", datas);
 
 const port = process.env.PORT || 5000;
